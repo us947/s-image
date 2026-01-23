fcdad5a665fd05fd464556df10b78e9fe9003d91
lbm <us781819@gmail.com>
2026-01-23 17:58:24 +0530

    Update icon URL in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: 'https://jxechgirxrbrblyrrqmt.supabase.co/storage/v1/object/public/images/bb5b5ced-6b47-425c-aad2-065017342a96/1768574759761-development.png',
+        url: 'https://jxechgirxrbrblyrrqmt.supabase.co/storage/v1/object/public/images/bb5b5ced-6b47-425c-aad2-065017342a96/1769171018448-Create_a_clean_202601231749.gif',
         
       },
       
